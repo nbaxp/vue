f49a6364a4f7b80f10a622f9e1c3a00723c7dc8b
wanggang <76527413@qq.com>
2021-11-29 17:19:58 +0800

    update

---
+++ examples/arco-design-vue/components/c-table.vue
@@ -0,0 +1,20 @@
+<template>
+    <a-pagination
+        show-total
+        hide-on-single-page
+        :total="modelValue.total"
+        :current="modelValue.current"
+        @change="modelValue.current=$event"
+        @pageSizeChange="modelValue.pageSize=$event"
+    />
+</template>
+<script>
+export default {
+    props: ["modelValue"],
+    emits:["update:modelValue"],
+    setup(){
+        return {
+        }
+    }
+};
+</script>
